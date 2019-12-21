90a08984ed20efa86cca55093282fc22aaea23fa
Tristan Kerr <50528889+Takerr3@users.noreply.github.com>
2019-12-20 23:17:25 -0500

    Update PubRecord.cpp

--- PubRecord.cpp
+++ PubRecord.cpp
@@ -1,8 +1,8 @@
 // Tristan Kerr
 // OOP244 NBB
-// takerr3@myseneca.ca
-// 022975148
-// Milestone 4
+// December 2019
+// https://www.linkedin.com/in/tristan-a-kerr/
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cstring>
